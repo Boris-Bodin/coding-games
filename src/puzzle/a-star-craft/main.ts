a977c3a8a178062210cf751bbdf5583208350138
Boris-Bodin <126199492+Boris-Bodin@users.noreply.github.com>
2024-03-26 11:42:20 +0100

    Update main.ts

--- src/puzzle/a-star-craft/main.ts
+++ src/puzzle/a-star-craft/main.ts
@@ -11,10 +11,10 @@ declare interface Array<T> {
 const HEIGHT: number = 10;
 const WIDTH: number = 19;
 
-type Direction = "U" | "D" | "L" | "R";
-type CellType = "." | "#" | Direction;
+type Direction = 'U' | 'D' | 'L' | 'R';
+type CellType = '.' | '#' | Direction;
 type Grid = Array<Array<Cell>>;
-type Coordinates = { x: number; y: number };
+type Coordinates = {x: number; y: number};
 
 class Cell {
     type: CellType;
@@ -25,12 +25,12 @@ class Cell {
         this.position = position;
     }
 
-    public isArrow(): this is Cell & { type: Direction } {
-        return this.type === "U" || this.type === "D" || this.type === "L" || this.type === "R";
+    public isArrow(): this is Cell & {type: Direction} {
+        return this.type === 'U' || this.type === 'D' || this.type === 'L' || this.type === 'R';
     }
 
     public isPlateform(): boolean {
-        return this.type === ".";
+        return this.type === '.';
     }
 }
 
@@ -38,14 +38,13 @@ class Robot {
     public position: Coordinates;
     public direction: Direction;
     public isAlive: boolean = true;
-    public historique: Array<Coordinates & { direction: Direction }> = [];
+    public historique: Array<Coordinates & {direction: Direction}> = [];
 
-    constructor() {
-    }
+    constructor() {}
 
     public init(): void {
-        const inputs: string[] = readline().split(" ");
-        this.position = { x: parseInt(inputs[0]), y: parseInt(inputs[1]) };
+        const inputs: string[] = readline().split(' ');
+        this.position = {x: parseInt(inputs[0]), y: parseInt(inputs[1])};
         this.direction = inputs[2] as Direction;
     }
 
@@ -57,7 +56,7 @@ class Robot {
         if (cell.isArrow()) {
             this.direction = cell.type;
         }
-        this.historique.push({ x: this.position.x, y: this.position.y, direction: this.direction });
+        this.historique.push({x: this.position.x, y: this.position.y, direction: this.direction});
         this.position = this.getNextPosition(this.position, this.direction);
 
         this.checkAlive(grid);
@@ -66,17 +65,17 @@ class Robot {
     private getNextPosition(position: Coordinates, direction: Direction): Coordinates {
         let newPosition: Coordinates = null;
         switch (direction) {
-            case "U":
-                newPosition = { x: position.x, y: position.y - 1 };
+            case 'U':
+                newPosition = {x: position.x, y: position.y - 1};
                 break;
-            case "D":
-                newPosition = { x: position.x, y: position.y + 1 };
+            case 'D':
+                newPosition = {x: position.x, y: position.y + 1};
                 break;
-            case "L":
-                newPosition = { x: position.x - 1, y: position.y };
+            case 'L':
+                newPosition = {x: position.x - 1, y: position.y};
                 break;
-            case "R":
-                newPosition = { x: position.x + 1, y: position.y };
+            case 'R':
+                newPosition = {x: position.x + 1, y: position.y};
                 break;
         }
         if (newPosition.x < 0) {
@@ -96,11 +95,11 @@ class Robot {
 
     private checkAlive(grid: Grid): void {
         const cell: Cell = grid[this.position.y][this.position.x];
-        if (cell.type === "#") {
+        if (cell.type === '#') {
             this.isAlive = false;
             return;
         }
-        this.isAlive = !this.historique.some((position: Coordinates & { direction: Direction }) => {
+        this.isAlive = !this.historique.some((position: Coordinates & {direction: Direction}) => {
             return position.x === this.position.x && position.y === this.position.y && position.direction === this.direction;
         });
     }
@@ -108,17 +107,18 @@ class Robot {
 
 class Board {
     public grid: Grid = [];
+    public initialArrowPositions: Array<Coordinates> = [];
     public robots: Array<Robot> = [];
     public score: number = 0;
 
-    constructor() {
-    }
+    constructor() {}
 
     public init(): void {
         for (let y = 0; y < HEIGHT; y++) {
             let row: Array<Cell> = readline()
-            .split("")
-            .map((value, x) => new Cell(value as CellType, { x, y }));
+                .split('')
+                .map((value, x) => new Cell(value as CellType, {x, y}));
+            row.filter(this.isArrow).forEach((cell: Cell) => this.initialArrowPositions.push(cell.position));
             this.grid.push(row);
         }
 
@@ -135,11 +135,11 @@ class Board {
         board.grid = this.grid.map((row: Array<Cell>) => row.map((cell: Cell) => new Cell(cell.type, cell.position)));
         board.robots = this.robots.map((robot: Robot) => {
             const newRobot: Robot = new Robot();
-            newRobot.position = { ...robot.position };
+            newRobot.position = {...robot.position};
             newRobot.direction = robot.direction;
             newRobot.isAlive = robot.isAlive;
-            newRobot.historique = robot.historique.map((position: Coordinates & { direction: Direction }) => ({
-                ...position
+            newRobot.historique = robot.historique.map((position: Coordinates & {direction: Direction}) => ({
+                ...position,
             }));
             return newRobot;
         });
@@ -158,7 +158,10 @@ class Board {
     }
 
     public getSolution(): Array<Cell> {
-        return this.grid.flatMap((row: Array<Cell>) => row).filter(this.isArrow);
+        return this.grid
+            .flatMap((row: Array<Cell>) => row)
+            .filter(this.isArrow)
+            .filter((cell: Cell) => !this.initialArrowPositions.includes(cell.position));
     }
 
     private isArrow(cell: Cell): boolean {
@@ -167,8 +170,7 @@ class Board {
 }
 
 class GameState {
-    constructor(public board: Board) {
-    }
+    constructor(public board: Board) {}
 
     public findBestSolution(): Board {
         const defaultBoard: Board = this.board.clone();
@@ -176,21 +178,20 @@ class GameState {
         let maxBoard = defaultBoard;
 
         const findLastPositionOfAllRobots: Array<Cell> = defaultBoard.robots.map((robot: Robot) => {
-            const historique: Array<Coordinates & { direction: Direction }> = robot.historique.slice(0);
+            const historique: Array<Coordinates & {direction: Direction}> = robot.historique.slice(0);
             let cellHisto = null;
             do {
                 cellHisto = historique.pop();
-
-            } while (this.board.grid[cellHisto.y][cellHisto.x].isPlateform() && historique.length > 0);
-            return new Cell(cellHisto.direction, { x: cellHisto.x, y: cellHisto.y });
+            } while (!this.board.grid[cellHisto.y][cellHisto.x].isPlateform() && historique.length > 0);
+            return new Cell(cellHisto.direction, {x: cellHisto.x, y: cellHisto.y});
         });
 
         const findAllPossibleArrows: Array<Cell> = findLastPositionOfAllRobots.flatMap((cell: Cell) => {
             return [
-                new Cell("U", { x: cell.position.x, y: cell.position.y }),
-                new Cell("D", { x: cell.position.x, y: cell.position.y }),
-                new Cell("L", { x: cell.position.x, y: cell.position.y }),
-                new Cell("R", { x: cell.position.x, y: cell.position.y })
+                new Cell('U', {x: cell.position.x, y: cell.position.y}),
+                new Cell('D', {x: cell.position.x, y: cell.position.y}),
+                new Cell('L', {x: cell.position.x, y: cell.position.y}),
+                new Cell('R', {x: cell.position.x, y: cell.position.y}),
             ];
         });
 
@@ -208,13 +209,13 @@ class GameState {
 
     public printSolution(): string {
         return this.board
-                   .getSolution()
-                   .map((cell: Cell) => `${cell.position.x} ${cell.position.y} ${cell.type}`)
-                   .join(" ");
+            .getSolution()
+            .map((cell: Cell) => `${cell.position.x} ${cell.position.y} ${cell.type}`)
+            .join(' ');
     }
 }
 
-const MAX_ARROW = 5;
+const MAX_ARROW = 2;
 
 const BOARD: Board = new Board();
 BOARD.init();
